09db80dcd9df30e0af851697f33475e07951b58f
David Cancel <dcancel@dcancel.com>
2012-04-23 19:02:22 -0300

    Add support for Unicode encoded strings in Flickr photo titles and descriptions.

--- import.py
+++ import.py
@@ -15,6 +15,32 @@ from openphoto import OpenPhoto
 
 from os.path import join, getsize
 
+# _decode_list() and _decode_data() adds support for Unicode strings in Flickr photo titles and descriptions.
+# These functions came from this post: http://stackoverflow.com/a/6633651/1232882
+def _decode_list(data):
+    rv = []
+    for item in data:
+        if isinstance(item, unicode):
+            item = item.encode('utf-8')
+        elif isinstance(item, list):
+            item = _decode_list(item)
+        elif isinstance(item, dict):
+            item = _decode_dict(item)
+        rv.append(item)
+    return rv
+
+def _decode_dict(data):
+    rv = {}
+    for key, value in data.iteritems():
+        if isinstance(key, unicode):
+           key = key.encode('utf-8')
+        if isinstance(value, unicode):
+           value = value.encode('utf-8')
+        elif isinstance(value, list):
+           value = _decode_list(value)
+        elif isinstance(value, dict):
+           value = _decode_dict(value)
+        rv[key] = v
 
 # main program
 def import_into_openphoto(client):
@@ -38,7 +64,7 @@ def import_into_openphoto(client):
 
       shutil.move(infile, "errored/%s" % i)
 
-      params = json.loads(json_str)
+      params = json.loads(json_str, object_hook=_decode_dict)
       resp = client.post('/photo/upload.json', params)
       result = json.loads(resp)
       if result['code'] == 201:
